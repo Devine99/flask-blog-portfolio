7f3a4a9afb35e93037b0e040841cbe5641a5039c
Devine99 <enukorahdevine@gmail.com>
2025-11-20 11:03:14 -0500

    Update mail config settings

--- app/config.py
+++ app/config.py
@@ -22,9 +22,13 @@ class Config:
     MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
     MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
     MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True").lower() == "true"
+    MAIL_USE_SSL = False
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.getenv("MAIL_USERNAME")
+    MAIL_DEBUG = False
+    MAIL_MAX_EMAILS = None
+    MAIL_ASCII_ATTACHMENTS = False
 
     # CKEditor
     CKEDITOR_SERVE_LOCAL = True
